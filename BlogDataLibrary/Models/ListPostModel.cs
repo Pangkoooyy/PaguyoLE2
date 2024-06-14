ea7611034463e2906167658261621b52da3caf21
Marc Francis A Paguyo <132049725+Pangkoooyy@users.noreply.github.com>
2024-06-14 14:34:40 +0800

    Initial commit

---
+++ BlogDataLibrary/Models/ListPostModel.cs
@@ -0,0 +1,11 @@
+﻿public class ListPostModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    public DateTime DateCreated { get; set; }
+    public string Username { get; set; }
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+
+}
